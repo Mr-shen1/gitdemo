168269d537897de3d654817d17ee58ccb552d3f2
skf <652920502@qq.com>
2022-04-17 21:14:45 +0800

    test1

---
+++ gittest/src/main/java/com/skf/git/test1/Test1.java
@@ -0,0 +1,15 @@
+package com.skf.git.test1;
+
+/**
+ * desc:
+ *
+ * @author: skf
+ * @date: 2022/04/17
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("哈哈");
+
+    }
+}
